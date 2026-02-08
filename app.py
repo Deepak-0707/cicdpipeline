e8259ce8b8d525490b6c3c194f9257ba6e1ae694
Deepak M <157140082+Deepak-0707@users.noreply.github.com>
2026-02-08 16:55:08 +0530

    modifed the code back to stable version

--- app.py
+++ app.py
@@ -1,13 +1,14 @@
 from http.server import BaseHTTPRequestHandler, HTTPServer
 
-"class SimpleHandler(BaseHTTPRequestHandler):
+class SimpleHandler(BaseHTTPRequestHandler):
     def do_GET(self):
         self.send_response(200)
         self.send_header("Content-type", "text/plain")
         self.end_headers()
-        self.wfile.write(b"Hello from version 1. Application is running successfully.\n")"
+        self.wfile.write(b"Hello from version 1. Application is running successfully.\n")
 
 if __name__ == "__main__":
     server = HTTPServer(("0.0.0.0", 8080), SimpleHandler)
     print("Starting server on port 8080")
     server.serve_forever()
+
